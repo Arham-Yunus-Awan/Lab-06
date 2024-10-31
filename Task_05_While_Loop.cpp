f4b90037defd24357260dca2d640692c40b098e1
Arham-Yunus-Awan <150243373+Arham-Yunus-Awan@users.noreply.github.com>
2024-10-31 16:44:16 +0500

    Create Task_05_While_Loop.cpp

---
+++ Task_05_While_Loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int i = 1;
+  while (i <= 5){
+      cout << "Row " << i << ": ";
+      int j = 1;
+      while (j <= 10){
+          cout << "Seat " << j << "  ";
+          j++;
+      }
+      cout << endl;
+      i++;
+  }
+  return 0;
+}
